aeed2fa609a45f7edfc5085b18cecaf5624c20d7
Kacper <contact@vg-electronics.com>
2026-06-17 12:33:45 +0200

    adjust route name for sanctum

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', fn() => response()->json(['status' => 'OK']));
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
